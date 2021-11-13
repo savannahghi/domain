1d20eb798fea78cad31dc2e2cec027db141d4bc7
Abiud <abiudrn@gmail.com>
2021-11-13 10:43:52 +0300

    fix: add content and suggested groups api specs
    - add fetchRecentContent api spec
    - add suggestedGroups api spec
    - fix avatar image urls

--- sdl/lambdas.js
+++ sdl/lambdas.js
@@ -54,7 +54,7 @@ async function login({ graphql }) {
     }
 }
 
-async function listTerms({ graphql }) {
+async function getCurrentTerms({ graphql }) {
     return {
         "text": "terms will be available here soon",
         "flavour": "CONSUMER"
@@ -89,51 +89,127 @@ async function sendOTP({ graphql }) {
 }
 
 
-async function verifyOTP() {
+async function verifyOTP({ graphql }) {
     return {
         "response": true,
-        "code": "4"
+        "code": null
     }
 }
 
-async function fetchContent() {
+async function fetchRecentContent({ graphql }) {
     return [{
-        "title": "Tips on how to keep yourself healthy",
-        "body": "<h1>Content will be available here soon",
-        "author": "Abiud Orina",
-        "authorAvatar": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
-        "heroImage": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
-        "contentType": "ARTICLE",
-        "publicLink": "https://bewell.co.ke/",
-        "isNew": true,
-        "estimate": "5 minutes",
-        "tags": ["Recommended", "Health", "Fitness"],
-        "createdAt": "2021-08-23T06:42:05.085216Z",
+            "title": "Tips on how to keep yourself healthy",
+            "body": "<h1>Content will be available here soon",
+            "author": "Abiud Orina",
+            "authorAvatar": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
+            "heroImage": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
+            "contentType": "ARTICLE",
+            "publicLink": "https://bewell.co.ke/",
+            "isNew": true,
+            "estimate": "5 minutes",
+            "tags": ["Recommended", "Health", "Fitness"],
+            "createdAt": "2021-08-23T06:42:05.085216Z",
+        }, {
+            "title": "Tips for better and healthy living",
+            "body": "<h1>Content will be available here soon",
+            "author": "Abiud Orina",
+            "authorAvatar": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
+            "heroImage": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
+            "contentType": "ARTICLE",
+            "publicLink": "https://bewell.co.ke/",
+            "isNew": true,
+            "estimate": "5 minutes",
+            "tags": ["Recommended", "Health", "Fitness"],
+            "createdAt": "2021-08-23T06:42:05.085216Z",
+        },
+        {
+            "title": "How to keep fit",
+            "body": "<h1>Content will be available here soon",
+            "author": "Abiud Orina",
+            "authorAvatar": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
+            "heroImage": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
+            "contentType": "ARTICLE",
+            "publicLink": "https://bewell.co.ke/",
+            "isNew": true,
+            "estimate": "5 minutes",
+            "tags": ["Recommended", "Health", "Fitness"],
+            "createdAt": "2021-08-23T06:42:05.085216Z",
+        },
+    ]
+}
+
+async function fetchContent({ graphql }) {
+    return [{
+            "title": "Tips on how to keep yourself healthy",
+            "body": "<h1>Content will be available here soon",
+            "author": "Abiud Orina",
+            "authorAvatar": "https://i.postimg.cc/9XpbrC25/profile-image.png",
+            "contentType": "ARTICLE",
+            "publicLink": "https://bewell.co.ke/",
+            "isNew": true,
+            "estimate": "5 minutes",
+            "tags": ["Recommended", "Health", "Fitness"],
+            "createdAt": "2021-08-23T06:42:05.085216Z",
+        }, {
+            "title": "Tips for better and healthy living",
+            "body": "<h1>Content will be available here soon",
+            "author": "Abiud Orina",
+            "authorAvatar": "https://i.postimg.cc/9XpbrC25/profile-image.png",
+            "contentType": "ARTICLE",
+            "publicLink": "https://bewell.co.ke/",
+            "isNew": true,
+            "estimate": "5 minutes",
+            "tags": ["Recommended", "Health", "Fitness"],
+            "createdAt": "2021-08-23T06:42:05.085216Z",
+        },
+        {
+            "title": "How to keep fit",
+            "body": "<h1>Content will be available here soon",
+            "author": "Abiud Orina",
+            "authorAvatar": "https://i.postimg.cc/9XpbrC25/profile-image.png",
+            "contentType": "ARTICLE",
+            "publicLink": "https://bewell.co.ke/",
+            "isNew": true,
+            "estimate": "5 minutes",
+            "tags": ["Recommended", "Health", "Fitness"],
+            "createdAt": "2021-08-23T06:42:05.085216Z",
+        },
+    ]
+}
+
+
+async function fetchSuggestedGroups({ graphql }) {
+    return [{
+        "name": "Ruaraka",
+        "avatar": "1.3k"
     }, {
-        "title": "Tips on how to keep yourself healthy",
-        "body": "<h1>Content will be available here soon",
-        "author": "Abiud Orina",
-        "authorAvatar": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
-        "heroImage": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
-        "contentType": "ARTICLE",
-        "publicLink": "https://bewell.co.ke/",
-        "isNew": true,
-        "estimate": "5 minutes",
-        "tags": ["Recommended", "Health", "Fitness"],
-        "createdAt": "2021-08-23T06:42:05.085216Z",
+        "name": "Mental health",
+        "avatar": "13k"
+    }, {
+        "name": "Ruaka self help",
+        "avatar": "1.2k"
+    }, {
+        "name": "Ruiru",
+        "avatar": "1.8k"
+    }, {
+        "name": "Kasarani",
+        "avatar": "400"
     }, ]
 }
 
 self.addGraphQLResolvers({
     // Queries
     "Query.login": login,
-    "Query.listTerms": listTerms,
+    "Query.getCurrentTerms": getCurrentTerms,
     "Query.setUserPIN": genericBoolResponse,
     "Query.getSecurityQuestions": getSecurityQuestions,
     "Query.sendOTP": sendOTP,
     "Query.resendOTP": sendOTP,
     "Query.verifyOTP": verifyOTP,
+    "Query.fetchRecentContent": fetchRecentContent,
     "Query.fetchContent": fetchContent,
+    "Query.setNickName": genericBoolResponse,
+    "Query.fetchSuggestedGroups": fetchSuggestedGroups,
 
     /// Mutations
     "Mutation.setUserPIN": genericBoolResponse,
